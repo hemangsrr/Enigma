2fcae24af742f965ca02ce482bb1cdb861ccc083
Hemang Mohan <44643029+hemangsrr@users.noreply.github.com>
2020-05-08 19:03:26 +0530

    env update

--- config/settings/common.py
+++ config/settings/common.py
@@ -103,7 +103,7 @@ MANAGERS = ADMINS
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres://postgres:margs@localhost/enigma'),
+    'default': env.db('DATABASE_URL', default='postgres://Hemang:hemang@localhost/enigma'),
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
